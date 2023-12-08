02d43ca4e0609ac341f789037a9a6126a3bbd021
ameyflash <amey.hema@gmail.com>
2023-12-07 18:44:55 -0700

    Added pulse_app

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'c17c9ee71a395cd8cd8cc01ce8ffd868d586c0ec'
+AESD_ASSIGNMENTS_VERSION = '79f7e88213b7dd1463ca8012ce61347cef9f756d'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
@@ -16,11 +16,15 @@ AESD_ASSIGNMENTS_GIT_SUBMODULES = YES
 
 define AESD_ASSIGNMENTS_BUILD_CMDS
 	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/temp_sensor all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/spi_test all
+	$(MAKE) $(TARGET_CONFIGURE_OPTS) -C $(@D)/pulse_sensor all
 endef
 
 # TODO add your temperature and heart rate sensor utilities/scripts to the installation steps below
 define AESD_ASSIGNMENTS_INSTALL_TARGET_CMDS
 	$(INSTALL) -m 0755 $(@D)/temp_sensor/temp_app $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/spi_test/spidev_test $(TARGET_DIR)/usr/bin
+	$(INSTALL) -m 0755 $(@D)/pulse_sensor/pulse_app $(TARGET_DIR)/usr/bin
 endef
 
 $(eval $(generic-package))
